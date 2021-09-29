a125ff6af3cf42ebb7e6c10e7e8ef259e1199954
Engr. Md. Muslim Uddin Arju <mdarju240@gmail.com>
2021-09-29 16:02:36 +0100

    blog sorting to show new one on first

--- src/components/blog/blog-list/index.jsx
+++ src/components/blog/blog-list/index.jsx
@@ -1,20 +1,30 @@
-import React, { Component } from "react";
+import React, {useState, useEffect} from "react";
 import { Row, Col } from "react-bootstrap";
 import BlogItem from "../blog-item";
-import posts from "../../../data/posts.json";
+import axios from 'axios'
 
-export default class BlogList extends Component {
-  render() {
-    return (
-      <>
-      <Row>
-        {posts.map((post) => (
-          <Col md={4} style={{ marginBottom: 50 }}>
-            <BlogItem key={post.title} {...post} />
-          </Col>
-        ))}
-      </Row>
-      </>
-    );
+const BlogList = () => {
+  const [post, setPosts] = useState();
+
+  useEffect(() => {
+    getPost()
+  }, [])
+  
+  const getPost = async () =>{
+    const response=await axios.get(`${process.env.REACT_APP_API_URL}/blogPosts/`);
+    setPosts(response.data)
   }
+  return (
+    <>
+    <Row>
+      {post &&  post.sort((a,b) => a.createdAt <  b.createdAt? 1 : -1).map((post) => (
+        <Col md={4} style={{ marginBottom: 50 }}>
+          <BlogItem key={post.title} {...post} />
+        </Col>
+      ))}
+    </Row>
+    </>
+  );
 }
+
+export default BlogList;
