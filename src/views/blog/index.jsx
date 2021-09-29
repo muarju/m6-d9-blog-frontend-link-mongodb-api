32dfe29c7d00f757588ffe8f93cad1ffd632d295
Engr. Md. Muslim Uddin Arju <mdarju240@gmail.com>
2021-09-29 16:00:47 +0100

    blogs data coming from api

--- src/views/blog/index.jsx
+++ src/views/blog/index.jsx
@@ -1,56 +1,58 @@
-import React, { Component } from "react";
+import React, { useState,useEffect } from "react";
 import { Container, Image } from "react-bootstrap";
 import { withRouter } from "react-router";
 import BlogAuthor from "../../components/blog/blog-author";
-import posts from "../../data/posts.json";
 import "./styles.css";
 import NavBar from '../../components/navbar'
-class Blog extends Component {
-  state = {
-    blog: {},
-    loading: true,
-  };
-  componentDidMount() {
-    const { id } = this.props.match.params;
-    console.log(posts);
-    const blog = posts.find((post) => post._id.toString() === id);
-    if (blog) {
-      this.setState({ blog, loading: false });
-    } else {
-      this.props.history.push("/404");
+import axios from 'axios'
+import { useHistory } from 'react-router-dom';
+
+
+const Blog = (props) => {
+
+  const { id } = props.match.params;
+  const history = useHistory();
+  const [posts, setPosts] = useState();
+
+  useEffect(() => {
+    getPost()
+  }, [])
+  
+  const getPost = async () =>{
+    const response=await axios.get(`${process.env.REACT_APP_API_URL}/blogPosts/`+id);
+    if(response){
+      setPosts(response.data)
+    }else{
+      history.push('/404')
     }
   }
+console.log(posts)
+ 
 
-  render() {
-    const { loading, blog } = this.state;
-    if (loading) {
-      return <div>loading</div>;
-    } else {
+
       return (
         <>
         <NavBar></NavBar>
         <div className="blog-details-root">
-          <Container>
-            <Image className="blog-details-cover" src={blog.cover} fluid />
-            <h1 className="blog-details-title">{blog.title}</h1>
+         {posts &&  <Container>
+            <Image className="blog-details-cover" src={posts.cover} fluid />
+            <h1 className="blog-details-title">{posts.title}</h1>
 
             <div className="blog-details-container">
               <div className="blog-details-author">
-                <BlogAuthor {...blog.author} />
+                <BlogAuthor {...posts.author} />
               </div>
               <div className="blog-details-info">
-                <div>{blog.createdAt}</div>
-                <div>{`${blog.readTime.value} ${blog.readTime.unit} read`}</div>
+                <div>{posts.createdAt}</div>
+                <div>{`${posts.readTime.value} ${posts.readTime.unit} read`}</div>
               </div>
             </div>
 
-            <div dangerouslySetInnerHTML={{ __html: blog.content }}></div>
-          </Container>
+            <div dangerouslySetInnerHTML={{ __html: posts.content }}></div>
+          </Container>}
         </div>
         </>
       );
-    }
   }
-}
 
 export default withRouter(Blog);
